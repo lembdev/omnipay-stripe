057fbed0a425da4233e546a1d30b9b7b7f89a65d
Alexander Vyzhanov <lembdev@gmail.com>
2017-08-05 17:07:40 +0300

    Fix refund problem

--- src/Gateway.php
+++ src/Gateway.php
@@ -15,7 +15,7 @@ class Gateway extends \Omnipay\Stripe\Gateway
      *
      * @return Message\CreateAccountRequest
      */
-    public function createAccount(array $parameters = array())
+    public function createAccount(array $parameters = [])
     {
         return $this->createRequest(Message\CreateAccountRequest::class, $parameters);
     }
@@ -27,7 +27,7 @@ class Gateway extends \Omnipay\Stripe\Gateway
      *
      * @return Message\CreateAccountRequest
      */
-    public function fetchAccount(array $parameters = array())
+    public function fetchAccount(array $parameters = [])
     {
         return $this->createRequest(Message\FetchAccountRequest::class, $parameters);
     }
@@ -39,7 +39,7 @@ class Gateway extends \Omnipay\Stripe\Gateway
      *
      * @return Message\CreateAccountRequest
      */
-    public function updateAccount(array $parameters = array())
+    public function updateAccount(array $parameters = [])
     {
         return $this->createRequest(Message\UpdateAccountRequest::class, $parameters);
     }
@@ -51,8 +51,18 @@ class Gateway extends \Omnipay\Stripe\Gateway
      *
      * @return Message\DeleteAccountRequest
      */
-    public function deleteAccount(array $parameters = array())
+    public function deleteAccount(array $parameters = [])
     {
         return $this->createRequest(Message\DeleteAccountRequest::class, $parameters);
+    }
+
+    /**
+     * @param array $parameters
+     *
+     * @return Message\RefundRequest
+     */
+    public function refund(array $parameters = [])
+    {
+        return $this->createRequest(Message\RefundRequest::class, $parameters);
     }
 }
---
+++ src/Message/RefundRequest.php
@@ -0,0 +1,94 @@
+<?php
+/**
+ * @author    Alexander Vyzhanov <lembdev@gmail.com>
+ * @copyright 2017 Astwellsoft <astwellsoft.com>
+ */
+
+namespace lembdev\Stripe\Message;
+
+use Omnipay\Common\Exception\InvalidRequestException;
+
+class RefundRequest extends \Omnipay\Stripe\Message\RefundRequest
+{
+    /**
+     * String indicating the reason for the refund.
+     * If set, possible values are duplicate, fraudulent, and requested_by_customer.
+     * Specifying fraudulent as the reason when you believe the charge
+     * to be fraudulent will help us improve our fraud detection algorithms.
+     *
+     * @param string $value
+     *
+     * @return AbstractRequest Provides a fluent interface
+     */
+    public function setReason($value)
+    {
+        return $this->setParameter('reason', $value);
+    }
+
+    /**
+     * Validates and returns the reason.
+     *
+     * @throws InvalidRequestException on any validation failure.
+     * @return string|null
+     */
+    public function getReason()
+    {
+        $reason = $this->getParameter('reason');
+
+        if ($reason === null) {
+            return null;
+        }
+
+        $possibleReasons = [
+            'duplicate',
+            'fraudulent',
+            'requested_by_customer',
+        ];
+
+        if (!in_array($reason, $possibleReasons)) {
+            throw new InvalidRequestException("Incorrect reason: {$reason}");
+        }
+
+        return $reason;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getData()
+    {
+        $this->validate('transactionReference');
+
+        $data = [];
+
+        if ($this->getAmount()) {
+            $data['reason'] = $this->getAmountInteger();
+        }
+
+        if ($this->getMetadata()) {
+            $data['metadata'] = $this->getMetadata();
+        }
+
+        if ($this->getReason()) {
+            $data['reason'] = $this->getReason();
+        }
+
+        if ($this->getReverseTransfer()) {
+            $data['reverse_transfer'] = 'true';
+        }
+
+        if ($this->getRefundApplicationFee()) {
+            $data['refund_application_fee'] = 'true';
+        }
+
+        return $data;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getEndpoint()
+    {
+        return $this->endpoint . '/refunds';
+    }
+}
